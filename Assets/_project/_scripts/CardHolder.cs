758bd97d8bae62bbf1f99b3253de35097f46bf25
Youssef Gaieb <73967591+youssef20886@users.noreply.github.com>
2025-11-22 15:49:29 +0100

    flipping card sequence

--- Assets/_project/_scripts/CardHolder.cs
+++ Assets/_project/_scripts/CardHolder.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CardHolder : MonoBehaviour
 {
     #region Variables Decleration
-    Card card;
+    private bool isFaceUp;
+    private float flipSpeed = 0.3f;
+    private Sequence flipSequence;
+
     #endregion
 
     #region Iinitialization
     public void Initialize()
     {
-        
     }
+    #endregion
+
+    #region Main Functionality
+    public void FlipCardWithSwap(System.Action onHalfWay)
+    {
+        if (flipSequence != null && flipSequence.IsActive() && flipSequence.IsPlaying())
+            return;
+
+        flipSequence = DOTween.Sequence();
+
+        flipSequence.Append(transform.DORotate(new Vector3(0, 90, 0), flipSpeed / 2))
+           .AppendCallback(() => onHalfWay?.Invoke())
+           .Append(transform.DORotate(new Vector3(0, 180, 0), flipSpeed / 2));
+    }
+
     #endregion
 }

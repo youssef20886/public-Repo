1159efd41f6a85427a631f755d4bf99e64113485
Youssef Gaieb <73967591+youssef20886@users.noreply.github.com>
2025-11-22 16:43:35 +0100

    more cards visuals+ hover effect on cards

--- Assets/_project/_scripts/CardHolder.cs
+++ Assets/_project/_scripts/CardHolder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -6,13 +7,29 @@ using UnityEngine;
 public class CardHolder : MonoBehaviour
 {
     #region Variables Decleration
+    public Sprite cardImage;
+
     private bool isFaceUp;
     private float flipSpeed = 0.3f;
     private Sequence flipSequence;
+    private Sprite cardBackground;
+    private SpriteRenderer spriteRenderer;
 
+    [Header("Hover Settings")]
+    public float hoverScale = 1.2f;
+    public float tweenDuration = 0.15f;
+    private Vector3 originalScale;
+    private Tween currentTween;
     #endregion
 
     #region Iinitialization
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        cardBackground = spriteRenderer.sprite;
+        originalScale = transform.localScale;
+    }
     public void Initialize()
     {
     }
@@ -23,15 +40,15 @@ public class CardHolder : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            FlipCardWithSwap(()=> Debug.Log("swap sprite"));
+            FlipCardWithSwap(() => SwapSprite());
         }
     }
-    public void FlipCardWithSwap(System.Action onHalfWay)
+    public void FlipCardWithSwap(Action onHalfWay)
     {
         if (flipSequence != null && flipSequence.IsActive() && flipSequence.IsPlaying())
             return;
-            
-        float targetY = isFaceUp ? 0 : 180;
+
+        float targetY = isFaceUp ? 180 : 0;
         flipSequence = DOTween.Sequence();
 
         flipSequence.Append(transform.DORotate(new Vector3(0, 90, 0), flipSpeed / 2))
@@ -39,6 +56,27 @@ public class CardHolder : MonoBehaviour
            .Append(transform.DORotate(new Vector3(0, targetY, 0), flipSpeed / 2));
 
         isFaceUp = !isFaceUp;
+    }
+
+    private void SwapSprite()
+    {
+        spriteRenderer.sprite = isFaceUp ? cardImage : cardBackground;
+    }
+
+    private void OnMouseEnter()
+    {
+        currentTween?.Kill();
+
+        currentTween = transform.DOScale(originalScale * hoverScale, tweenDuration)
+                                .SetEase(Ease.OutQuad);
+    }
+
+    private void OnMouseExit()
+    {
+        currentTween?.Kill();
+
+        currentTween = transform.DOScale(originalScale, tweenDuration)
+                                .SetEase(Ease.OutQuad);
     }
 
     #endregion

234d29f6b56381f1b5f87c85661ebb61e8f2dc8e
Youssef Gaieb <73967591+youssef20886@users.noreply.github.com>
2025-11-22 15:54:19 +0100

    card flip improvement

--- Assets/_project/_scripts/CardHolder.cs
+++ Assets/_project/_scripts/CardHolder.cs
@@ -19,16 +19,26 @@ public class CardHolder : MonoBehaviour
     #endregion
 
     #region Main Functionality
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FlipCardWithSwap(()=> Debug.Log("swap sprite"));
+        }
+    }
     public void FlipCardWithSwap(System.Action onHalfWay)
     {
         if (flipSequence != null && flipSequence.IsActive() && flipSequence.IsPlaying())
             return;
-
+            
+        float targetY = isFaceUp ? 0 : 180;
         flipSequence = DOTween.Sequence();
 
         flipSequence.Append(transform.DORotate(new Vector3(0, 90, 0), flipSpeed / 2))
            .AppendCallback(() => onHalfWay?.Invoke())
-           .Append(transform.DORotate(new Vector3(0, 180, 0), flipSpeed / 2));
+           .Append(transform.DORotate(new Vector3(0, targetY, 0), flipSpeed / 2));
+
+        isFaceUp = !isFaceUp;
     }
 
     #endregion
